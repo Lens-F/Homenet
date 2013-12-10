e6e71bcde68f929221192688672192fd7193205c
NOAH <tnoah12@gmail.com>
2013-12-10 15:27:50 -0500

    Add website html and css
    Added entire sites html and css files

---
+++ search.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<title>Search results</title>
+  	<meta charset="utf-8">
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <script src="js/jquery-1.7.1.min.js"></script>
+    <script src="js/script.js"></script>
+    <script src="search/search.js"></script>
+<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+<!--[if lt IE 9]>
+	<script src="js/html5.js"></script>
+	<link rel="stylesheet" href="css/ie.css"> 
+<![endif]-->
+</head>
+<body>
+	<div class="main">
+    <!-- Header -->
+        <header>
+        	<nav>
+              <h1><a href="index.html"><img src="images/logo.png" alt=""></a><span>creative furniture</span></h1>
+              <ul class="sf-menu">
+                  <li><a href="index.html">About Us</a></li>
+                  <li><a href="index-1.html">Portfolio</a></li>
+                  <li><a href="index-2.html">Services</a>
+                  	<ul>
+                        <li><a href="more.html">support</a></li>
+                        <li><a href="more.html">tips</a></li>
+                        <li><a href="more.html">consulting</a>
+                        	<ul>
+                                <li><a href="more.html">on-line</a></li>
+                                <li><a href="more.html">publications</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                  </li>
+                  <li><a href="index-3.html">Useful Links</a></li>
+                  <li><a href="index-4.html">Contacts</a></li>
+              </ul> 
+              <div class="clear"></div> 
+             </nav>  
+       </header>
+    <!-- Content -->
+        <section id="content" class="content">
+          <div class="container_24">
+            <div class="grid_24">
+                <h3>Search result:</h3>
+                <div id="search-results"></div>
+            </div>
+ 	    	<div class="clear"></div> 
+          </div>  
+        </section>  
+    </div>
+<!-- Footer -->
+	<footer>
+    	Luxus © 2012 | <a href="index-5.html">Privacy policy</a>   
+    </footer>
+<!-- Footer -->
+</body>
+</html>
